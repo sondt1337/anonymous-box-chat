4f881afef76cfaf14af764fbbc57ff300c765749
Son Dinh Thai <87920408+iamironman1233@users.noreply.github.com>
2022-08-22 16:52:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-const express = require('express')
-let PORT = process.env.PORT || 3000
-const app = express()
-const http = require('http')
-const server = http.createServer(app)
-const {
-     Server 
-} = require('socket.io')
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
 
-const io = new Server(server)
+const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
-})
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
-io.on('connection', (socket) => {
-  console.log('user connected')
-  socket.on('on-chat', data => {
-    io.emit('user-chat', data)
-  })
-})
+io.on("connection", (socket) => {
+  console.log("user connected");
+  socket.on("on-chat", (data) => {
+    io.emit("user-chat", data);
+  });
+});
 
 server.listen(3000, () => {
-  console.log('listening on port 3000')
-})
+  console.log("listening on port 3000");
+});
