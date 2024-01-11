4c7b7405331971b7b4a3657923492d703acca243
Son Dinh Thai <sondinh99999@gmail.com>
2024-01-11 22:32:51 +0700

    cập nhật giao diện

--- index.js
+++ index.js
@@ -6,17 +6,19 @@ const { Server } = require("socket.io");
 
 const io = new Server(server);
 
+app.use(express.static('public'));
+
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + "/index.html");
 });
 
 io.on("connection", (socket) => {
-  console.log("user connected");
-  socket.on("on-chat", (data) => {
-    io.emit("user-chat", data);
-  });
+    console.log("user connected");
+    socket.on("on-chat", (data) => {
+        io.emit("user-chat", data);
+    });
 });
 
-server.listen(3000, () => {
-  console.log("listening on port 3000");
+server.listen(8000, () => {
+    console.log("listening on port 8000");
 });
